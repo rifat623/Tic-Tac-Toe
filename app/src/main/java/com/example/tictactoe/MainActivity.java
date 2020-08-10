92973a9bbd541752c2c3a84bffdf5c552f362045
rifat623 <30624687+rifat623@users.noreply.github.com>
2020-08-09 19:10:48 -0500

    v3

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int roundCount;
 
     private int player1Points;
-    private int palyer2Points;
+    private int player2Points;
 
     private TextView textViewPlayer1 = (TextView) findViewById(R.id.text_view_p1);
     private TextView textViewPlayer2 = (TextView) findViewById(R.id.text_view_p2);
@@ -34,11 +35,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Reference grid buttons
         for (int i = 0; i < 3; i ++) {
-            for (int j = 0 ; j < 3; j++);
+            for (int j = 0 ; j < 3; j++) {
                 String buttonID = "button_" + i + j;
-                int resID = getResources().getIdentifier(buttonID, 'id', getPackageName());
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
+            }
         }
 
         // Reference reset button
@@ -46,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                gameReset();
             }
         });
     }
@@ -112,15 +114,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return false;
     }
 
+    //if both players draw
     private void draw() {
-
+        Toast.makeText(this, "Draw!!", Toast.LENGTH_SHORT).show();
+        resetBoard();
     }
 
+    //update point for player 1 and display message
     private void player1Wins() {
+        player1Points++;
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    //update point for player 2 and display message
+    private void player2Wins() {
+        player2Points++;
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void updatePointsText(){
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+
+    private void resetBoard(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
 
     }
 
-    private void player2Wins() {
+    private void gameReset() {
+        player1Points = 0;
+        player2Points = 0;
+        updatePointsText();
+        resetBoard();
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
     }
 }
